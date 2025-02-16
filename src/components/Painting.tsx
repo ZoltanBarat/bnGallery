d9e1e5ca60d8e0c3f8b6f1d36e62f19d88b5516c
Zoltán Barát <barat.zoltan@gmail.com>
2025-02-16 10:52:50 +0100

    change date format if its 0

--- src/components/Painting.tsx
+++ src/components/Painting.tsx
@@ -9,19 +9,34 @@ export default function Painting({ item }: { item: dataItem }) {
     <>
       {zoomed && (
         <div className="zoomed-wrapper" onClick={() => setZoomed(false)}>
-          <img className="art-card-image zoomed-image roll-out" src={item.image} alt="A painting made by Nikolett Bohus" />
+          <img
+            className="art-card-image zoomed-image roll-out"
+            src={item.image}
+            alt="A painting made by Nikolett Bohus"
+          />
         </div>
       )}
       <div className="art-card roll-out">
-        <div className="art-card-image-container" onClick={() => setZoomed(true)} style={{ width: item.width * 10, height: item.height * 10 }}>
+        <div
+          className="art-card-image-container"
+          onClick={() => setZoomed(true)}
+          style={{ width: item.width * 10, height: item.height * 10 }}
+        >
           <span></span>
           <span></span>
           <span></span>
           <span></span>
-          <img className="art-card-image image-shadow" src={item.small} alt="A painting made by Nikolett Bohus" loading="lazy" />
+          <img
+            className="art-card-image image-shadow"
+            src={item.small}
+            alt="A painting made by Nikolett Bohus"
+            loading="lazy"
+          />
           <div className="art-card-text-container">
             <h3 className="art-card-text-title">{item.name}</h3>
-            <p className="art-card-text-date">{item.date}</p>
+            <p className="art-card-text-date">
+              {item.date == "0" ? "-" : item.date}
+            </p>
           </div>
         </div>
       </div>
